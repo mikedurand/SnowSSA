c96386f2d5edda8c592e823a5a047dfd56a07585
Mike Durand <durand.8@osu.edu>
2020-04-30 07:46:21 -0400

    Comment writes to final files
    Added write-outs of the layer-by-layer comments to the final files

--- writeSSAcsvs.m
+++ writeSSAcsvs.m
@@ -5,22 +5,29 @@ for i=1:length(SSADataset)
     
     disp(['Writing ' fname '...']);
     
+    for k=1:length(SSADataset(i).HdrLines)
+        SSADataset(i).HdrLines{k}=strip(SSADataset(i).HdrLines{k},',');
+    end
+    
     DataToWrite=[SSADataset(i).HdrLines]';
     
     j=length(DataToWrite);
     
-    j=j+1;  DataToWrite{j}='#,,,,,';
-    j=j+1;  DataToWrite{j}='# Sample signal (mV), Reflectance (%), Specific surface area (SSA), Top Depth (cm), Do (mm), Comments';    
+    j=j+1;  DataToWrite{j}='#';
+    j=j+1;  DataToWrite{j}='# Sample signal (mV), Reflectance (%), Specific surface area (SSA), Height (cm), Do (mm), Comments';    
              
     for k=1:length(SSADataset(i).SSA)
         DataLine=[SSADataset(i).Voltage(k) SSADataset(i).Reflectance(k) SSADataset(i).SSA(k) SSADataset(i).Depth(k) SSADataset(i).Do(k)];
         
         j=j+1;
         DataToWrite(j)={sprintf('%.1f,%.2f,%.0f,%.1f,%f',DataLine)};
+        if ~isempty(SSADataset(i).Comments{k})
+            DataToWrite(j)={[DataToWrite{j} ',' SSADataset(i).Comments{k}]};
+        end
     end
     writecell(DataToWrite,fname,'QuoteStrings',false);
 end
 
-disp(['Done. Read ' num2str(length(SSADataset)) ' files.'])
+disp(['Done. Wrote ' num2str(length(SSADataset)) ' files.'])
 
 end
